fcac2684029e27af253e24a3ef8075d1f27a0a28
Jesus Guerrero <rocha.jesus@hotmail.com>
2016-10-18 15:12:16 -0700

    refactored Article routes & setup angular (ng) to manage articles

--- server/routes/articles/index.js
+++ server/routes/articles/index.js
@@ -18,7 +18,7 @@ var mongoose = require('mongoose'),
 
 module.exports = function(server) {
   [{
-    method: ['GET','POST'],
+    method: 'GET',
     path: '/articles/{link?}',
     config: {
       handler: function ( request, reply ) {
@@ -29,11 +29,67 @@ module.exports = function(server) {
             if( !article ) {
               reply.view('articles/not-found');
             } else {
-              reply.view('articles/one', {
-                article: article
+              if( request.query.type === 'json') {
+                reply({article: article});
+              } else {
+                reply.view('articles/one', {
+                  article: article
+                });
+              }
+            }
+          });
+        } else {
+          Article.find( ).exec( function(err, articles) {
+            if( err ) { return reply( Boom.badRequest(err) ); }
+
+            if( request.query.type === 'json' ) {
+              reply({articles: articles});
+            } else {
+              reply.view('articles/index', {
+                articles: articles
               });
             }
           });
+        }
+      }
+    }
+  }, {
+    method: ['GET','POST','PUT','DELETE'],
+    path: '/api/articles/{id?}',
+    config: {
+      handler: function( request, reply ) {
+        if( request.params.id ) {
+          if( request.method === 'put' ) {
+            delete request.payload.article._id;
+            Article.update({_id: request.params.id}, request.payload.article, {
+              upsert: false
+            }).exec( function(err, article){
+              if( err ) {
+                if( 11000 === err.code || 11001 === err.code ) {
+                  return reply( Boom.badRequest("Duplicate key error index") );
+                } else {
+                  return reply( Boom.badRequest(err) );
+                }
+              }
+              if( article && article.ok && article.nModified ) {
+                reply({updated: true});
+              } else {
+                reply({updated: false});
+              }
+            });
+          } else if (request.method === 'delete') {
+            Article.remove({_id: request.params.id}).exec( function(err, article) {
+              if( err ) { return reply( Boom.badRequest(err) ); }
+
+              if( article && article.result && article.result.ok ) {
+                reply({deleted: true});
+              } else {
+                reply({deleted: false});
+              }
+            });
+          } else {
+            reply( Boom.badRequest('method not supported') );
+          }
         } else {
           if( request.method === 'post' ) {
             Article.create( request.payload.article, function ( err ) {
@@ -46,54 +102,11 @@ module.exports = function(server) {
               }
               reply({created: true});
             });
+          } else if( request.method === 'get' ) {
+            reply.view('articles/ng');
           } else {
-            Article.find().exec( function(err, articles) {
-              if( err ) { return reply( Boom.badRequest(err) ); }
-
-              if( request.query.type === 'json' ) {
-                reply({articles: articles});
-              } else {
-                reply.view('articles/index', {
-                  articles: articles
-                });
-              }
-            });
+            reply( Boom.badRequest('method not supported') );
           }
-        }
-      }
-    }
-  }, {
-    method: ['PUT','DELETE'],
-    path: '/api/articles/{id?}',
-    config: {
-      handler: function( request, reply ) {
-        if( request.method === 'put' ) {
-          Article.update({_id: request.params.id}, request.payload.article, {
-            upsert: false
-          }).exec( function(err, article){
-            if( err ) {
-              if( 11000 === err.code || 11001 === err.code ) {
-                return reply( Boom.badRequest("Duplicate key error index") );
-              } else {
-                return reply( Boom.badRequest(err) );
-              }
-            }
-            if( article && article.ok && article.nModified ) {
-              reply({updated: true});
-            } else {
-              reply({updated: false});
-            }
-          });
-        } else {
-          Article.remove({_id: request.params.id}).exec( function(err, article) {
-            if( err ) { return reply( Boom.badRequest(err) ); }
-
-            if( article && article.result && article.result.ok ) {
-              reply({deleted: true});
-            } else {
-              reply({deleted: false});
-            }
-          });
         }
       }
     }
